e162410a743735676306c9292fd4ef52327fb921
Simon Souter <simonsouter@gmail.com>
2015-05-20 10:30:45 +0100

    a

--- src/main/scala/com/boldradius/sdf/akka/Receiver.scala
+++ src/main/scala/com/boldradius/sdf/akka/Receiver.scala
@@ -4,12 +4,8 @@ import akka.actor._
 
 import scala.collection.mutable
 
-object Receiver {
-  def props = Props[Receiver]
-}
-
 // Mr Dummy Consumer simply shouts to the log the messages it receives
-class Receiver extends Actor with ActorLogging {
+class Receiver(statsActor: ActorRef) extends Actor with ActorLogging {
 
   private val actorsBySession = mutable.HashMap.empty[Long, ActorRef]
 
@@ -35,3 +31,8 @@ class Receiver extends Actor with ActorLogging {
     newTracker
   }
 }
+
+object Receiver {
+  def props(statsActor: ActorRef): Props = Props(new Receiver(statsActor))
+}
+
--- src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
@@ -1,9 +1,13 @@
 package com.boldradius.sdf.akka
 
-import akka.actor.{Actor, ActorLogging}
+import akka.actor.{Props, Actor, ActorLogging}
 
 class StatsActor extends Actor with ActorLogging {
   override def receive: Receive = {
     case _ => log.info("Stat received!")
   }
+}
+
+object StatsActor {
+  def props: Props = Props(new StatsActor)
 }
