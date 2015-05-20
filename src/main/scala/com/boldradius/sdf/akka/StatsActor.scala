d6419612d4b4d9d7187c7a6c300f0f326f81a618
Simon Souter <simonsouter@gmail.com>
2015-05-20 15:30:03 +0100

    a

--- src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/StatsActor.scala
@@ -8,7 +8,10 @@ import scala.collection.mutable
 
 class StatsActor extends Actor with ActorLogging {
 
-  var stats = new Stats()
+  val requestsPerBrowser = new mutable.HashMap[String, Int]() withDefaultValue 0
+  val hitsPerMinute = new mutable.HashMap[Int, Int]() withDefaultValue 0
+  val pageVisitDistribution = new mutable.HashMap[String, Int]() withDefaultValue 0
+  val referrerDistribution = new mutable.HashMap[String, Int]() withDefaultValue 0
 
   override def receive: Receive = {
     case StatsDump(requests) =>
@@ -17,51 +20,72 @@ class StatsActor extends Actor with ActorLogging {
         calculateRequestsPerBrowser(request.browser)
         calculateHitsPerMinute(request.timestamp)
         calculatePageVisitDistribution(request.url)
-
-        top3LandingPages()
-        top2Browsers()
-        //        calculateAvVisitTimePerURL()
+        calculateReferrerDistribution(request.referrer)
       })
 
     case _ => log.info("Stat received!")
   }
 
-  def top3LandingPages(): Seq[String] = {
-    val sorted = stats.pageVisitDistribution.toSeq.sortWith(_._2 > _._2)
-    sorted.slice(0,3).map(_._1)
+  //(Minute->count)
+  def getBusiestMinuteOfDay: (Int,Int) = {
+    def sorted = hitsPerMinute.toSeq.sortWith(_._2 > _._2)
+    sorted.head
   }
 
-  def top3SinkPages() = {
+  def getPageVisitDistribution(): Map[String, Double] = {
+    val total = totalVisits
+    pageVisitDistribution.map(pageCount => {
+      (pageCount._1, pageCount._2.toDouble / total)
+    }).toMap
+  }
 
+  private def totalVisits: Int = {
+    pageVisitDistribution.foldLeft(0)(_ + _._2)
+  }
+
+  def top3LandingPages(): Seq[String] = {
+    val sorted = pageVisitDistribution.toSeq.sortWith(_._2 > _._2)
+    sorted.slice(0, 3).map(_._1)
+  }
+
+  //TODO sink
+  def top3SinkPages() = {
+//    val sinkPages = stats.requestsPerBrowser.filter()
   }
 
   def top2Browsers() = {
-    val sorted = stats.requestsPerBrowser.toSeq.sortWith(_._2 > _._2)
-    sorted.slice(0,3).map(_._1)
+    val sorted = requestsPerBrowser.toSeq.sortWith(_._2 > _._2)
+    sorted.slice(0, 3).map(_._1)
   }
 
   def top2Referrers() = {
-
+    val sorted = referrerDistribution.toSeq.sortWith(_._2 > _._2)
+    sorted.slice(0, 2).map(_._1)
   }
 
   private[akka] def calculateRequestsPerBrowser(browser: String) = {
-    val requestPerBrowser = stats.requestsPerBrowser(browser) + 1
-    stats.requestsPerBrowser.update(browser, requestPerBrowser)
+    val requestPerBrowser = requestsPerBrowser(browser) + 1
+    requestsPerBrowser.update(browser, requestPerBrowser)
     log.info(s"Updated requestsPerBrowser, for [${browser}] to: [${requestPerBrowser}]")
   }
 
   private[akka] def calculateHitsPerMinute(timestamp: Long) = {
     val minuteOfDay = new DateTime(timestamp).getMinuteOfDay
-    val count = stats.hitsPerMinute(minuteOfDay) + 1
-    stats.hitsPerMinute.update(minuteOfDay, count)
+    val count = hitsPerMinute(minuteOfDay) + 1
+    hitsPerMinute.update(minuteOfDay, count)
     log.info(s"Updated busiestMinuteOfTheDate, for min: [${minuteOfDay}] to count: [${count}]")
   }
 
   private[akka] def calculatePageVisitDistribution(url: String) = {
-
-    val newCount = stats.pageVisitDistribution(url) + 1
-    stats.pageVisitDistribution.update(url, newCount)
+    val newCount = pageVisitDistribution(url) + 1
+    pageVisitDistribution.update(url, newCount)
     log.info(s"Updated page count for url:[${url}] to [${newCount}]")
+  }
+
+  private[akka] def calculateReferrerDistribution(referrer: String) = {
+    val newCount = referrerDistribution(referrer) + 1
+    referrerDistribution.update(referrer, newCount)
+    log.info(s"Updated referrer count for referrer:[${referrer}] to [${newCount}]")
   }
 
   //  private[akka] def calculateAvVisitTimePerURL() = {
@@ -69,47 +93,6 @@ class StatsActor extends Actor with ActorLogging {
   //  }
 
 }
-
-class Stats() {
-  val requestsPerBrowser = new mutable.HashMap[String, Int]() withDefaultValue 0
-  val hitsPerMinute = new mutable.HashMap[Int, Int]() withDefaultValue 0
-  val pageVisitDistribution = new mutable.HashMap[String, Int]() withDefaultValue 0
-
-}
-
-////case class RequestsPerBrowser(count: Int, av: Double)
-//
-//class PageVisitDistribution {
-//  val pageCounts =
-//
-//  def addPageCount(url: String, count: Int) = {
-//    pageCounts.update(url, pageCounts(url) + count)
-//  }
-//
-//  def %(url: String) = {
-//    pageCounts(url) / totalCount
-//  }
-//
-//  def totalCount:Int = {
-//    pageCounts.values.sum
-//  }
-//}
-
-//object StatsFunctions {
-//  def calcRequestsPerBrowser(curr: RequestsPerBrowser, no: Int): RequestsPerBrowser = {
-//    val newCount = curr.count + no
-//    val newAverage: Double = (curr.count * curr.av + no) / newCount
-//    RequestsPerBrowser(newCount, newAverage)
-//  }
-
-//}
-
-//AvverageVisitTimePerURL
-//Top3LandingPages (#hits)
-//Top3SinkPages (#hits)
-//Top2Browsers (#hits)
-//Top2Referrers (#hits)
-
 
 object StatsActor {
   def props: Props = Props(new StatsActor)
