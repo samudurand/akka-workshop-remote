1b2337c342eea1648942b7b7dbd0fc749ff13ccd
samudurand <samueld@cakesolutions.net>
2015-05-20 10:36:14 +0100

    timeout

--- src/main/scala/com/boldradius/sdf/akka/UserTrackerActor.scala
+++ src/main/scala/com/boldradius/sdf/akka/UserTrackerActor.scala
@@ -1,9 +1,10 @@
 package com.boldradius.sdf.akka
 
-import akka.actor.{Props, ActorLogging, Actor}
+import akka.actor.{ReceiveTimeout, Props, ActorLogging, Actor}
 
 import scala.collection.parallel.mutable
 import scala.collection.mutable.ListBuffer
+import scala.concurrent.duration._
 
 object UserTrackerActor {
   def props = Props[UserTrackerActor]
@@ -13,9 +14,17 @@ class UserTrackerActor extends Actor with ActorLogging {
 
   private val requests = new ListBuffer[Request]
 
+  //Sets timeout
+  context.setReceiveTimeout(20 seconds)
+
   def receive: Receive = {
     case request: Request => requests += request
+    case ReceiveTimeout => closeSession()
     case _ => log.info("received")
+  }
+
+  def closeSession() = {
+    context.stop(self)
   }
 
 }
